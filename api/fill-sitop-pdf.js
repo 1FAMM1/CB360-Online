14de7dbb1fa80be234cf88cffb7af9506aab6cb4
GrupoCB360 <86024360+1FAMM1@users.noreply.github.com>
2025-10-17 00:24:57 +0100

    Change page orientation from portrait to landscape

--- api/fill-sitop-pdf.js
+++ api/fill-sitop-pdf.js
@@ -90,7 +90,7 @@ export default async function handler(req, res) {
 
         // Configuração de página
         sheet.pageSetup = {
-            orientation: 'portrait',
+            orientation: 'landscape',
             paperSize: 9,
             fitToPage: true,
             fitToWidth: 1,

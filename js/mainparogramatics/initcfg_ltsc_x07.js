25c56e3cb0ced17f29a837cc4a45a29970e29c01
GrupoCB360 <86024360+1FAMM1@users.noreply.github.com>
2025-11-28 22:28:42 +0000

    Update initcfg_ltsc_x07.js

--- js/mainparogramatics/initcfg_ltsc_x07.js
+++ js/mainparogramatics/initcfg_ltsc_x07.js
@@ -1,13 +1,7 @@
-const SUPABASE_URL = 'https://rjkbodfqsvckvnhjwmhg.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqa2JvZGZxc3Zja3ZuaGp3bWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNjM3NjQsImV4cCI6MjA2MzczOTc2NH0.jX5OPZkz1JSSwrahCoFzqGYw8tYkgE8isbn12uP43-0';
+const SUPABASE_PROXY = '/api/supabase-proxy?path=';
+
 function getSupabaseHeaders(options = {}) {
-      const headers = {
-        'apikey': SUPABASE_ANON_KEY,
-        'Authorization': `Bearer ${SUPABASE_ANON_KEY}`,
-        'Content-Type': 'application/json'
-      };
-      if (options.returnRepresentation) {
-        headers['Prefer'] = 'return=representation';
-      }
-      return headers;
-    }
+  const headers = { 'Content-Type': 'application/json' };
+  if (options.returnRepresentation) headers['Prefer'] = 'return=representation';
+  return headers;
+}

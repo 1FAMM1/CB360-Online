bf21f0bca181c0a662ab9645ea5d10b503d01583
GrupoCB360 <86024360+1FAMM1@users.noreply.github.com>
2025-12-19 16:27:13 +0000

    Update sendTelegram.js

--- api/mobile/sendTelegram.js
+++ api/mobile/sendTelegram.js
@@ -7,7 +7,7 @@
     const SUPABASE_URL = 'https://rjkbodfqsvckvnhjwmhg.supabase.co';
     const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqa2JvZGZxc3Zja3ZuaGp3bWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNjM3NjQsImV4cCI6MjA2MzczOTc2NH0.jX5OPZkz1JSSwrahCoFzqGYw8tYkgE8isbn12uP43-0';
     const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-    const TOKEN = '7966236313:AAEq2sHq7chCiEtDKttawgyjMxB-6McP9AA';
+    const TOKEN = '8596696700:AAGpN0uh_XPAjDkajIR-Wpey8_EkWFPjbPI';
     const apiRoute = nextConnect({
       onError(error, req, res) {
         res.status(500).json({ success: false, error: error.message });

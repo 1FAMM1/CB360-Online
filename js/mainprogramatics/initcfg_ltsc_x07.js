7b2ed424ec798352513e0db241f2bda2f2de2416
GrupoCB360 <86024360+1FAMM1@users.noreply.github.com>
2026-01-07 21:51:22 +0000

    Update initcfg_ltsc_x07.js

--- js/mainprogramatics/initcfg_ltsc_x07.js
+++ js/mainprogramatics/initcfg_ltsc_x07.js
@@ -1,13 +1,22 @@
 const SUPABASE_URL = 'https://rjkbodfqsvckvnhjwmhg.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqa2JvZGZxc3Zja3ZuaGp3bWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNjM3NjQsImV4cCI6MjA2MzczOTc2NH0.jX5OPZkz1JSSwrahCoFzqGYw8tYkgE8isbn12uP43-0';
 function getSupabaseHeaders(options = {}) {
-      const headers = {
-        'apikey': SUPABASE_ANON_KEY,
-        'Authorization': `Bearer ${SUPABASE_ANON_KEY}`,
-        'Content-Type': 'application/json'
-      };
-      if (options.returnRepresentation) {
-        headers['Prefer'] = 'return=representation';
-      }
-      return headers;
-    }
+  // Pegar o código da corporação (ex: "0805") e o NINT do utilizador
+  const corp = localStorage.getItem("currentCorpOperNr");
+  const nint = localStorage.getItem("currentCorpNint") || sessionStorage.getItem("currentNInt");
+
+  const headers = {
+    'apikey': SUPABASE_ANON_KEY,
+    'Authorization': `Bearer ${SUPABASE_ANON_KEY}`,
+    'Content-Type': 'application/json',
+    // ESTES SÃO OS HEADERS QUE O TEU SQL USA
+    'x-my-corpo': corp, 
+    'x-my-nint': nint
+  };
+
+  if (options.returnRepresentation) {
+    headers['Prefer'] = 'return=representation';
+  }
+  
+  return headers;
+}

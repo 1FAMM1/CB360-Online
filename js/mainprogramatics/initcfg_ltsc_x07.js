7229da7dbcb0d46a0a8c21271054a856203606c1
GrupoCB360 <86024360+1FAMM1@users.noreply.github.com>
2026-01-08 01:14:38 +0000

    Update initcfg_ltsc_x07.js

--- js/mainprogramatics/initcfg_ltsc_x07.js
+++ js/mainprogramatics/initcfg_ltsc_x07.js
@@ -2,7 +2,7 @@ const SUPABASE_URL = 'https://rjkbodfqsvckvnhjwmhg.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqa2JvZGZxc3Zja3ZuaGp3bWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNjM3NjQsImV4cCI6MjA2MzczOTc2NH0.jX5OPZkz1JSSwrahCoFzqGYw8tYkgE8isbn12uP43-0';
 function getSupabaseHeaders(options = {}) {
   // Pegar o código da corporação (ex: "0805") e o NINT do utilizador
-  const corp = localStorage.getItem("currentCorpOperNr");
+  const corp = localStorage.getItem("currentCorpOperNr") || sessionStorage.getItem("currentCorpOperNr"); 
   const nint = localStorage.getItem("currentCorpNint") || sessionStorage.getItem("currentNInt");
 
   const headers = {

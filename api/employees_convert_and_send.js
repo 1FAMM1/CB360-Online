ac7309b73dbb3a86e811c4637d376197344b6ba9
GrupoCB360 <86024360+1FAMM1@users.noreply.github.com>
2026-03-06 13:13:36 +0000

    Update employees_convert_and_send.js

--- api/employees_convert_and_send.js
+++ api/employees_convert_and_send.js
@@ -667,7 +667,7 @@
           dataRowsXml += makeRowXml(rowNum, emp);
         });
         for (let i = ROW_START + employees.length; i <= ROW_MAX; i++) {
-          dataRowsXml += `<row r="${i}" spans="2:9" ht="20" hidden="1" x14ac:dyDescent="0.25"><c r="B${i}" s="8"/><c r="C${i}" s="8"/><c r="D${i}" s="8"/><c r="E${i}" s="8"/><c r="F${i}" s="8"/><c r="G${i}" s="8"/><c r="H${i}" s="8"/><c r="I${i}" s="8"/></row>`;
+          dataRowsXml += `<row r="${i}" spans="2:9" ht="20" hidden="1" x14ac:dyDescent="0.25"><c r="B${i}" s="0"/><c r="C${i}" s="0"/><c r="D${i}" s="0"/><c r="E${i}" s="0"/><c r="F${i}" s="0"/><c r="G${i}" s="0"/><c r="H${i}" s="0"/><c r="I${i}" s="0"/></row>`;
         }
         const newSheetData = `<sheetData>${headerRows.join("")}${dataRowsXml}</sheetData>`;
         let newSheetXml = beforeSheetData + newSheetData + afterSheetData;

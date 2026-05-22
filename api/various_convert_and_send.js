82220ed83b6746a060445ae10d973e83f172b21c
GrupoCB360 <86024360+1FAMM1@users.noreply.github.com>
2026-05-22 14:07:11 +0100

    Destructure additional properties from data object

--- api/various_convert_and_send.js
+++ api/various_convert_and_send.js
@@ -366,7 +366,7 @@
             service: "gmail",
             auth: {user: process.env.GMAIL_EMAIL, pass: process.env.GMAIL_APP_PASSWORD},
           });
-          const { logoUrl, corpOperNr: corpNr } = data;
+          const {logoUrl, corpOperNr: corpNr, corpName, eventName, senderName = "Utilizador"} = data;
           const htmlAttendanceTemplate = `
             <!DOCTYPE html>
             <html>

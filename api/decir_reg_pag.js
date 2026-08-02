4b2184917c35048112313e58e1a3414e47bb0f91
GrupoCB360 <86024360+1FAMM1@users.noreply.github.com>
2026-08-02 14:21:18 +0100

    Fix URL for SBAOPAT registration template

--- api/decir_reg_pag.js
+++ api/decir_reg_pag.js
@@ -16,7 +16,7 @@
     const TEMPLATE_PAG_URL = "https://raw.githubusercontent.com/1FAMM1/CB360-Online/main/templates/decir_pag_template.xlsx";
     const TEMPLATE_CODE_A33_URL = "https://raw.githubusercontent.com/1FAMM1/CB360-Online/main/templates/cod_a33_template.xlsx";
     const TEMPLATE_ANEPC_URL = "https://raw.githubusercontent.com/1FAMM1/CB360-Online/main/templates/anepc_template.xlsx";
-    const TEMPLATE_REG_SBAOPAT_URL = "https://raw.githubusercontent.com/1FAMM1/CB360-Online/main/templates/sabopat_reg_template.xlsx";
+    const TEMPLATE_REG_SBAOPAT_URL = "https://raw.githubusercontent.com/1FAMM1/CB360-Online/main/templates/sbaopat_reg_template.xlsx";
 
 
     const TEMPLATE_OCORR_URL = "https://raw.githubusercontent.com/1FAMM1/CB360-Online/main/templates/reg_ocorr_decir.xlsx";
